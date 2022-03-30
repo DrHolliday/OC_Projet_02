3c63a88ba2a755223d46c42dfad848164a24af7f
Doc Holliday <renaud.ialacci@gmail.com>
2022-03-30 17:56:00 +0200

    ajout creation dossier image et check

--- produit.py
+++ produit.py
@@ -1,24 +1,26 @@
 #RI: Importation des packages necessaire au projet: 
 import requests
+#BSA pour beautifullsoup et requete sur tag html etc.
 from bs4 import BeautifulSoup
 import pandas
+# os pour la creation de dossier ( images)
+import os
 
-
-# RI: Fonction pour récupérer infos d'une page produit
-
-# •	product_page_url
+# ---- A FAIRE ------------------------------------------
+# •	ok product_page_url
 # •	ok universal_ product_code (upc)
 # •	ok title
 # •	ok price_including_tax
 # •	ok price_excluding_tax
 # •	ok number_available 
 # •	ok product_description
-# •	category
+# •	ok category
 # •	ok review_rating
 # •	ok image_url
+# -- Dossier Image et sauvegarde jpg
 
 
-
+# RI: Fonction pour récupérer infos d'une page produit
 
 def recupPageProduit():
     url = "http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html"
@@ -26,7 +28,9 @@ def recupPageProduit():
     reponse = requests.get(url)
     soup = BeautifulSoup(reponse.text, "html.parser")
 
-    #RI: Récupération des parties qui m'intéressent:
+    #RI: Récupération des parties qui m'intéressent et l'afficher/stocker:
+    productPageUrl = url
+    print("Url de la page: ",productPageUrl)
     prodType = soup.find("th", text="Product Type").find_next_sibling("td")
     print("Product Type:", prodType.text)
     upc = soup.find("th", text="UPC").find_next_sibling("td")
@@ -41,13 +45,24 @@ def recupPageProduit():
     print("Disponibilité: " , disponibilite.text)
     description = soup.find("div", id="product_description").find_next_sibling("p")
     print("Description du produit : " , description.text)
+    categorie = soup.find("ul", class_="breadcrumb").findChildren()[4].find("a").text.strip()
+    print("Catégorie: " , categorie)
     reviews = soup.find("th", text="Number of reviews").find_next_sibling("td")
     print("Number of reviews: " , reviews.text)
     image = soup.find("div", class_="item active").find("img").get("src")
     print("Image url: " , image)
 
-    categorie = soup.find("ul", class_="breadcrumb").findChildren()[2]
-    print(categorie)
+    #RI : Vérification dossier images (création si non existant) Sauvegarde de l illustration dans le dossier
+    dossierImages = "images"
+    isExist = os.path.exists(dossierImages)
+    if isExist == True:
+        print("Le dossier images existe!")
+        #RI : Alors enregistrer le fichier image dans le dossier images (voir si renommer fichier ?)
+    else:
+        print("pas de dossier Images")
+        #RI : Créer le dossier images
+        os.makedirs(dossierImages)
+        print("Dossier images créé")
 
 
 recupPageProduit()
