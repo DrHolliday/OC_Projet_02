996e8682d0a237ab107f1e6ef34fb97e7e5c4471
Doc Holliday <renaud.ialacci@gmail.com>
2022-03-30 15:35:03 +0200

    ajout code pour sélectionner les parties de la fiche produits

--- produit.py
+++ produit.py
@@ -4,41 +4,50 @@ from bs4 import BeautifulSoup
 import pandas
 
 
-# RI: Fonction pour récupérer infos d'une page produit ( http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html )
+# RI: Fonction pour récupérer infos d'une page produit
 
 # •	product_page_url
-# •	universal_ product_code (upc)
-# •	title
-# •	price_including_tax
-# •	price_excluding_tax
-# •	number_available
-# •	product_description
+# •	ok universal_ product_code (upc)
+# •	ok title
+# •	ok price_including_tax
+# •	ok price_excluding_tax
+# •	ok number_available 
+# •	ok product_description
 # •	category
-# •	review_rating
-# •	image_url
+# •	ok review_rating
+# •	ok image_url
 
 
 
 
 def recupPageProduit():
-    url = "http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
-    # url = input("Entrez l'url du produit")
+    url = "http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html"
+
     reponse = requests.get(url)
     soup = BeautifulSoup(reponse.text, "html.parser")
 
+    #RI: Récupération des parties qui m'intéressent:
+    prodType = soup.find("th", text="Product Type").find_next_sibling("td")
+    print("Product Type:", prodType.text)
+    upc = soup.find("th", text="UPC").find_next_sibling("td")
+    print("UPC: " , upc.text)
     titre = soup.find("h1")
-    table = soup.find("table", class_="table table-striped")
-    
     print("Titre: " + titre.text)
+    prixSansTaxe = soup.find("th", text="Price (excl. tax)").find_next_sibling("td")
+    print("Prix (sans taxe): " , prixSansTaxe.text)
+    prixAvecTaxe = soup.find("th", text="Price (excl. tax)").find_next_sibling("td")
+    print("Prix (avec taxe): " , prixAvecTaxe.text)
+    disponibilite = soup.find("th", text="Availability").find_next_sibling("td")
+    print("Disponibilité: " , disponibilite.text)
+    description = soup.find("div", id="product_description").find_next_sibling("p")
+    print("Description du produit : " , description.text)
+    reviews = soup.find("th", text="Number of reviews").find_next_sibling("td")
+    print("Number of reviews: " , reviews.text)
+    image = soup.find("div", class_="item active").find("img").get("src")
+    print("Image url: " , image)
 
-    #RI: Boucle pour cycler dans la table et extraire le contenu des tags: A travailler 28/03/22
-    elem = []
-    while elem in table:
-        upc = soup.find("th", text="UPC")
-        print("UPC: " , upc.find_next_sibling("td"))
-        prodType = soup.find("th", text="Product Type")
-        print("Product Type", prodType.find_next_sibling("td"))
-        print(elem)
+    categorie = soup.find("ul", class_="breadcrumb").findChildren()[2]
+    print(categorie)
 
 
 recupPageProduit()
